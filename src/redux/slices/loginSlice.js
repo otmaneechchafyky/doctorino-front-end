27b2c8b9695ceefc4037343a5fad43f0081e93f3
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-15 02:38:27 +0100

    Add reducers to manipulate login, logout and login failure

---
+++ src/redux/slices/loginSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  error: null,
+};
+
+const loginSlice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.isAuthenticated = true;
+      state.user = action.payload;
+      state.error = null;
+    },
+    loginFailure: (state, action) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.error = null;
+    },
+  },
+});
+
+export const { loginSuccess, loginFailure, logout } = loginSlice.actions;
+
+export default loginSlice.reducer;
