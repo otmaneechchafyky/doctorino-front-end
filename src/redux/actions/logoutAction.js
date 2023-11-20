7dc0a8b139e716117567813337e8c1b95de9ea39
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-20 15:11:35 +0100

    Add action to logout correctly

---
+++ src/redux/actions/logoutAction.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const logoutUser = () => {
+  return async () => {
+    try {
+      const authToken = localStorage.getItem('authToken');
+
+      const response = await axios.delete('http://127.0.0.1:4000/logout', {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      localStorage.removeItem('authToken');
+      console.log(response.data.message)
+      return response.data;
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+};
