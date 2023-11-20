445aa21bdcb74ab5ba7ff99c01a5e2cdb2f4e223
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-11-20 15:13:41 +0100

    Update sign up action

--- src/redux/actions/signUpAction.js
+++ src/redux/actions/signUpAction.js
@@ -6,8 +6,8 @@ const url = 'http://127.0.0.1:4000/signup';
 export const addNewUser = createAsyncThunk('users/addNewUser', async (userData) => {
     try {
       const resp = await axios.post(url, {user: userData});
-      return resp.data;
+      return resp.data.status.message;
     } catch (error) {
-      return error.message;
+      return error;
     }
 });
