54f624fa6533a1194f4cd5a7ca47fd5ce33ec645
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-12-19 03:52:12 +0100

    Update App.js to include the new routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import SignUpForm from './components/forms/SignUpForm'
 import LoginForm from './components/forms/LoginForm';
 import Home from './routes/Home'
+import Animals from './routes/Animals'
+import Vets from './routes/Vets'
+import Appointments from './routes/Appointments'
+import NewAnimal from './components/forms/NewAnimal'
+import NewVet from './components/forms/NewVet'
+import NewAppointment from './components/forms/NewAppointment'
 import './App.css';
 
 function App() {
@@ -11,6 +17,12 @@ function App() {
         <Route path='/' element={<LoginForm />} />
         <Route path='/home' element={<Home />} />
         <Route path='/signup' element={<SignUpForm />} />
+        <Route path='/home/animals' element={<Animals />} />
+        <Route path='/home/vets' element={<Vets />} />
+        <Route path='/home/appointments' element={<Appointments />} />
+        <Route path='/new_animal' element={<NewAnimal />} />
+        <Route path='/new_vet' element={<NewVet />} />
+        <Route path='/new_appointment' element={<NewAppointment />} />
       </Routes>
     </div>
   );
