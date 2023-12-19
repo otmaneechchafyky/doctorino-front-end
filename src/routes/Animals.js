186d2212c9e28615869aeb4ac72a5d6ff5d982ba
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-12-19 04:17:50 +0100

    Update animals route to fetch animals in each mount

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { fetchAnimal } from '../redux/actions/animalActions';
+import { useDispatch } from 'react-redux';
 
 const Animals = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchAnimal());
+  }, [dispatch]);
+
   return (
     <div>
-      Animals List
+      <br />
       <Link to='/new_animal'>Add Animal</Link>
     </div>
   )
