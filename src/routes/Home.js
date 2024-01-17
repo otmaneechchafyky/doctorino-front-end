285ac2a9cab45420c909b9018caa660f1cafcfe6
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-17 02:24:26 +0100

    Sepearate the home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,11 @@
-import { logoutUser } from '../redux/actions/logoutAction';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUser } from '../redux/actions/currentUserAction';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { logoutUser } from "../redux/actions/logoutAction";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser } from "../redux/actions/currentUserAction";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -19,44 +19,45 @@ const Home = () => {
     dispatch(fetchUser());
   }, [dispatch]);
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return <div>Loading...</div>;
   }
 
-  if (status === 'failed') {
+  if (status === "failed") {
     return <div>Error: {error}</div>;
   }
 
   const handleLogout = () => {
     dispatch(logoutUser());
-    toast.success('You Logged out successfully.', {
+    toast.success("You Logged out successfully.", {
       position: toast.POSITION.TOP_LEFT,
     });
     setTimeout(() => {
-      navigate('/');
+      navigate("/");
     }, 2000);
   };
 
   return (
-    <div>
-      <ul>
-        <Link to='animals'>Animals</Link>
-        <Link to='vets'>Vets</Link>
-        <Link to='appointments'>Appointments</Link>
-      </ul>
-      <button type="button" onClick={handleLogout}>
-        Logout
-      </button>
-      <br />
-      <div>
-      {currentUser && (
-        <div>
-          <p>User Name: {currentUser.userName}</p>
-          <p>Email: {currentUser.email}</p>
-          <p>Created Date: {currentUser.created_date}</p>
-        </div>
-      )}
-    </div>
+    <div className="flex w-full h-screen">
+      <nav className="bg-cyan-300 w-[10%]">
+        <Link to="animals">Animals</Link>
+        <Link to="vets">Vets</Link>
+        <Link to="appointments">Appointments</Link>
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
+      <div className="bg-indigo-700 flex flex-col w-[90%]">
+        <header className="bg-indigo-400">
+          {currentUser && (
+            <div>
+              <p>User Name: {currentUser.userName}</p>
+              <p>Email: {currentUser.email}</p>
+            </div>
+          )}
+        </header>
+        <div className="h-screen">home</div>
+      </div>
       <ToastContainer />
     </div>
   );
