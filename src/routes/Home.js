3a33f34d74534aa1af45837d65bbba40dd4c8d01
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-20 04:16:45 +0100

    Styling dashboard pt2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -45,39 +45,49 @@ const Home = () => {
 
   return (
     <div className="flex w-full h-screen">
-      <nav className="bg-cyan-300 w-[20%] flex flex-col gap-4">
+      <nav className="bg-teal-400 w-[20%] flex flex-col gap-12">
         <p className="h-[8rem] bg-yellow-200">LOGO</p>
-        <div className="flex flex-col gap-6 ml-4">
-        <Link to="animals">
-        <li className="list-none flex flex-row gap-2 justify-content items-center">
-        <IoMdHome className="w-[3rem] h-[3rem]" /><span className="text-lg font-bold">Home</span>
-        </li>
-      </Link>
-        <Link to="vets">
-        <li className="list-none flex flex-row gap-2 justify-content items-center">
-        <FaUserDoctor className="w-[3rem] h-[3rem]" /><span className="text-lg font-bold">Vets</span>
-        </li></Link>
-        <Link to="appointments">
-          <li className="list-none flex flex-row gap-2 justify-content items-center">
-        <AiFillCalendar className="w-[3rem] h-[3rem]" /><span className="text-lg font-bold">Appointments</span>
-        </li></Link>
-        <Link to='/new_appointment'>
-          <li className="list-none flex flex-row gap-2 justify-content items-center">
-        <IoIosAddCircleOutline className="w-[3rem] h-[3rem]" /><span className="text-lg font-bold">New app</span>
-        </li></Link>
-        <li className="list-none flex flex-row gap-2 justify-content items-center mt-[5rem] cursor-pointer" onClick={handleLogout}>
-          <MdLogout className="w-[3rem] h-[3rem]" />
-          <span className="text-lg font-bold">Log out</span>
-        </li>
+        <div className="flex flex-col">
+          <Link to="animals" className="group cursor-pointer hover:bg-teal-300 p-4">
+            <li className="list-none flex flex-row gap-2 justify-content items-center">
+              <IoMdHome className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900 group-hover:text-slate-900" />
+              <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900 group-hover:text-slate-900">Home</span>
+            </li>
+          </Link>
+          <Link to="vets" className="group cursor-pointer hover:bg-teal-300 p-4">
+            <li className="list-none flex flex-row gap-2 justify-content items-center">
+              <FaUserDoctor className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
+              <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">Vets</span>
+            </li>
+          </Link>
+          <Link to="appointments" className="group cursor-pointer hover:bg-teal-300 p-4">
+            <li className="list-none flex flex-row gap-2 justify-content items-center">
+              <AiFillCalendar className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
+              <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">Appointments</span>
+            </li>
+          </Link>
+          <Link to="/new_appointment" className="group cursor-pointer hover:bg-teal-300 p-4">
+            <li className="list-none flex flex-row gap-2 justify-content items-center">
+              <IoIosAddCircleOutline className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
+              <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">New appointment</span>
+            </li>
+          </Link>
+          <li
+            className="list-none flex flex-row gap-2 justify-content items-center mt-[1rem] group cursor-pointer hover:bg-teal-300 p-4"
+            onClick={handleLogout}
+          >
+            <MdLogout className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
+            <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">Log out</span>
+          </li>
         </div>
       </nav>
-      <div className="bg-indigo-700 flex flex-col w-[80%]">
-        <header className="bg-indigo-400">
+      <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400">
+        <header className="">
           {currentUser && (
             <div>
               <p>User Name: {currentUser.userName}</p>
               <p>Email: {currentUser.email}</p>
-              <img className="w-8" src={userIcon} alt="User Icon"/>
+              <img className="w-8" src={userIcon} alt="User Icon" />
             </div>
           )}
         </header>
