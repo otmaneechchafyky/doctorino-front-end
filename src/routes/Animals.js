1f54710771d961cc3308083a82e41b45cd54e1b7
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-21 04:06:13 +0100

    Add Navbar to Animals

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { fetchAnimal } from '../redux/actions/animalActions';
 import { useDispatch } from 'react-redux';
+import Navbar from '../components/Navbar';
 
 const Animals = () => {
 
@@ -12,9 +13,11 @@ const Animals = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <br />
-      <Link to='/new_animal'>Add Animal</Link>
+    <div className="flex w-full h-screen">
+      <Navbar />
+      <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
+        <Link to='/new_animal'>Add Animal</Link>
+      </div>  
     </div>
   )
 }
--- src/routes/Vets.js
+++ src/routes/Vets.js
@@ -8,8 +8,7 @@ const Vets = () => {
       <Navbar />
       <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
         <Link to='/new_vet'>Add Vet</Link>
-      </div>
-      
+      </div>  
     </div>
   )
 }
