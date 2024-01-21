d4ce3db3064989173c3012463bcc6e875903bc14
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-21 03:03:57 +0100

    Add Logo and style it

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import { MdLogout } from "react-icons/md";
 import "react-toastify/dist/ReactToastify.css";
 import userIcon from "../assets/user_icon.png";
+import choper from "../assets/doctorino.png";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -45,35 +46,37 @@ const Home = () => {
 
   return (
     <div className="flex w-full h-screen">
-      <nav className="bg-teal-400 w-[20%] flex flex-col gap-12">
-        <p className="h-[8rem] bg-yellow-200">LOGO</p>
+      <nav className="bg-teal-400 w-[20%] flex flex-col gap-6">
+        <div className="w-full flex items-center justify-center">
+        <img src={choper} alt="Choper" className="w-48 h-48"/>
+        </div>
         <div className="flex flex-col">
-          <Link to="animals" className="group cursor-pointer hover:bg-teal-300 p-4">
+          <Link to="animals" className="group cursor-pointer hover:bg-teal-300 p-2">
             <li className="list-none flex flex-row gap-2 justify-content items-center">
               <IoMdHome className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900 group-hover:text-slate-900" />
               <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900 group-hover:text-slate-900">Home</span>
             </li>
           </Link>
-          <Link to="vets" className="group cursor-pointer hover:bg-teal-300 p-4">
+          <Link to="vets" className="group cursor-pointer hover:bg-teal-300 p-2">
             <li className="list-none flex flex-row gap-2 justify-content items-center">
               <FaUserDoctor className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
               <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">Vets</span>
             </li>
           </Link>
-          <Link to="appointments" className="group cursor-pointer hover:bg-teal-300 p-4">
+          <Link to="appointments" className="group cursor-pointer hover:bg-teal-300 p-2">
             <li className="list-none flex flex-row gap-2 justify-content items-center">
               <AiFillCalendar className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
               <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">Appointments</span>
             </li>
           </Link>
-          <Link to="/new_appointment" className="group cursor-pointer hover:bg-teal-300 p-4">
+          <Link to="/new_appointment" className="group cursor-pointer hover:bg-teal-300 p-2">
             <li className="list-none flex flex-row gap-2 justify-content items-center">
               <IoIosAddCircleOutline className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
               <span className="text-lg font-bold text-slate-800 group-hover:text-slate-900">New appointment</span>
             </li>
           </Link>
           <li
-            className="list-none flex flex-row gap-2 justify-content items-center mt-[1rem] group cursor-pointer hover:bg-teal-300 p-4"
+            className="list-none flex flex-row gap-2 justify-content items-center mt-[1rem] group cursor-pointer hover:bg-teal-300 p-2"
             onClick={handleLogout}
           >
             <MdLogout className="w-[3rem] h-[3rem] text-slate-800 group-hover:text-slate-900" />
