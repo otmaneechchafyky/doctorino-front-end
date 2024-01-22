d8c56afd11b90d4b9090d696e231440c1ac3c434
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-22 23:46:24 +0100

    Update Appointments to have Header

--- src/routes/Appointments.js
+++ src/routes/Appointments.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from "../components/Navbar"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const Appointments = () => {
   return (
     <div className="flex w-full h-screen">
       <Navbar />
       <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
+        <Header />
         <Link to='/new_appointment'>Add appointment</Link>
       </div>
       
