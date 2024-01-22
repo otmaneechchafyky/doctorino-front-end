4e9183e80143d32ff67fcc6d2acc41b971938602
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-22 23:46:02 +0100

    Update Animals to have Header

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { fetchAnimal } from '../redux/actions/animalActions';
-import { useDispatch } from 'react-redux';
-import Navbar from '../components/Navbar';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchAnimal } from "../redux/actions/animalActions";
+import { useDispatch } from "react-redux";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const Animals = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAnimal());
@@ -16,10 +16,11 @@ const Animals = () => {
     <div className="flex w-full h-screen">
       <Navbar />
       <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
-        <Link to='/new_animal'>Add Animal</Link>
-      </div>  
+        <Header />
+        <Link to="/new_animal">Add Animal</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Animals
+export default Animals;
