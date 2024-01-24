a9e609926b3bc156bc734b9c5cfda3cc51cf4eaa
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-01-24 08:19:00 +0100

    Update Animals page and include loading page

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -1,16 +1,42 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { fetchAnimal } from "../redux/actions/animalActions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 
 const Animals = () => {
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.currentUser.status);
+  const error = useSelector((state) => state.currentUser.error);
 
   useEffect(() => {
     dispatch(fetchAnimal());
   }, [dispatch]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex w-full h-screen">
+        <Navbar />
+        <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
+          <Header />
+          <p>Loading ..</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (status === "failed") {
+    return (
+      <div className="flex w-full h-screen">
+        <Navbar />
+        <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
+          <Header />
+          <p>Loading ..</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex w-full h-screen">
