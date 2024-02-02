e141b4bdd3af46c052c1961abe0d63a099575cd1
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-02 03:41:09 +0100

    Adding slider to show animals

--- src/routes/Animals.js
+++ src/routes/Animals.js
@@ -4,6 +4,8 @@ import { fetchAnimal } from "../redux/actions/animalActions";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Animals = () => {
   const dispatch = useDispatch();
@@ -18,7 +20,9 @@ const Animals = () => {
     let filteredAnimals = [];
     if (magicWord === "Only my animals") {
       filteredAnimals = allAnimals.filter((key) => {
-        return animals[key].animal_owner.userName === currentUser.userData.userName;
+        return (
+          animals[key].animal_owner.userName === currentUser.userData.userName
+        );
       });
       setMyAnimals(filteredAnimals);
       setMagicWord("All animals");
@@ -26,9 +30,8 @@ const Animals = () => {
       filteredAnimals = allAnimals;
       setMyAnimals(filteredAnimals);
       setMagicWord("Only my animals");
-      return filteredAnimals;
     }
-  }
+  };
 
   useEffect(() => {
     dispatch(fetchAnimal());
@@ -50,8 +53,10 @@ const Animals = () => {
       <div className="flex w-full h-screen">
         <Navbar />
         <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
-        <Header title="Animals"/>
-        <p className="flex items-center justify-center h-[100%]">Loading ...</p>
+          <Header title="Animals" />
+          <p className="flex items-center justify-center h-[100%]">
+            Loading ...
+          </p>
         </div>
       </div>
     );
@@ -61,19 +66,29 @@ const Animals = () => {
     <div className="flex w-full h-screen">
       <Navbar />
       <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
-        <Header title="Animals"/>
-        <button type="button" onClick={filterMyAnimals}>{magicWord}</button>
-        <ul>
-        {myAnimals.map((key) => (
-            <li key={key}>
+        <Header title="Animals" />
+        <button type="button" onClick={filterMyAnimals}>
+          {magicWord}
+        </button>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="max-w-[80%] border border-white flex items-center justify-center"
+        >
+          {myAnimals.map((key) => (
+            <SwiperSlide key={key} className="border border-yellow-300">
               <img src={animals[key].data.animal_photo} alt="something" />
+              <div>
               <p>{animals[key].data.name}</p>
               <p>{animals[key].data.date_of_birth}</p>
               <p>{animals[key].data.escape_attempts}</p>
               <p>{animals[key].animal_owner.userName}</p>
-            </li>
+              </div>
+            </SwiperSlide>
           ))}
-        </ul>
+        </Swiper>
         <Link to="/new_animal">Add Animal</Link>
       </div>
     </div>
