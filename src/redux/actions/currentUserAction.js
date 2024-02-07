585bd6babc0c4e13fe251ebd4684d850bb1689ee
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-07 04:01:55 +0100

    Update base url in current user action

--- src/redux/actions/currentUserAction.js
+++ src/redux/actions/currentUserAction.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
     const authToken = localStorage.getItem('authToken');
-  const response = await axios.get("http://127.0.0.1:4000/current_user", {
+  const response = await axios.get("https://doctorinoapi.onrender.com/current_user", {
     headers: {
       Authorization: `Bearer ${authToken}`,
     },
