2c7e409b583e60f336363035c52edccbdb7a6eb8
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-07 04:01:22 +0100

    Update base url in sign up action

--- src/redux/actions/signUpAction.js
+++ src/redux/actions/signUpAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const url = 'http://127.0.0.1:4000/signup';
+const url = 'https://doctorinoapi.onrender.com/signup';
 
 export const addNewUser = createAsyncThunk('users/addNewUser', async (userData) => {
     try {
