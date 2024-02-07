8d69e59ba59b99fe91ce381e83d5d73853b4b369
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-07 04:00:39 +0100

    Update base url in login action

--- src/redux/actions/loginAction.js
+++ src/redux/actions/loginAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://doctorinoapi.onrender.com';
 
 export const loginUser = async (formData) => {
   try {
