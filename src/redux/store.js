0c4afc891cc08fc4d8c8be6e47adcf7432c2b935
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-10 07:10:26 +0100

    Update stpre to include animal producer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import signUpReducer from './slices/signUpSlice';
 import loginReducer from './slices/loginSlice';
 import userReducer from './slices/currentUserSlice';
+import animalReducer from './slices/animalSlice'
 
 const store = configureStore({
   reducer: {
     signup: signUpReducer,
     login: loginReducer,
     currentUser: userReducer,
+    animalsData: animalReducer
   },
 });
 
