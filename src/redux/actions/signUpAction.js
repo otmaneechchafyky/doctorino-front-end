c0b6c139d9f0e0cc09d7251f2b1ef1262a71e454
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-10 07:06:42 +0100

    Update signUpAction

--- src/redux/actions/signUpAction.js
+++ src/redux/actions/signUpAction.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-
-const url = 'http://127.0.0.1:4000/signup';
+import { apiURL } from "./URL";
 
 export const addNewUser = createAsyncThunk('users/addNewUser', async (userData) => {
     try {
-      const resp = await axios.post(url, {user: userData});
+      const resp = await axios.post(`${apiURL}/signup`, {user: userData});
       return resp.data.status.message;
     } catch (error) {
       return error;
