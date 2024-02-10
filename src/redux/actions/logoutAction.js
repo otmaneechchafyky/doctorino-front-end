43475c41b267a7b16d865aa5fdda8976197c7aec
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-10 07:06:20 +0100

    Update logoutAction

--- src/redux/actions/logoutAction.js
+++ src/redux/actions/logoutAction.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { apiURL } from "./URL";
 
 export const logoutUser = () => {
   return async () => {
     try {
       const authToken = localStorage.getItem('authToken');
 
-      const response = await axios.delete('http://127.0.0.1:4000/logout', {
+      const response = await axios.delete(`${apiURL}/logout`, {
         headers: {
           Authorization: `Bearer ${authToken}`,
         },
