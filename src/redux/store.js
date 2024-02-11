31a72a24072a2ce55e178ca5dc603730474330f1
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-11 06:50:09 +0100

    Update store to contain genresArray

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,15 @@ import signUpReducer from './slices/signUpSlice';
 import loginReducer from './slices/loginSlice';
 import userReducer from './slices/currentUserSlice';
 import animalReducer from './slices/animalSlice'
+import genreReducer  from "./slices/genreSlice";
 
 const store = configureStore({
   reducer: {
     signup: signUpReducer,
     login: loginReducer,
     currentUser: userReducer,
-    animalsData: animalReducer
+    animalsData: animalReducer,
+    genresData: genreReducer,
   },
 });
 
