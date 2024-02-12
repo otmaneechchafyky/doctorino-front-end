c36b8d7cc0669ceae9dc45668f019a4c2b7d475b
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-12 06:45:19 +0100

    Update animals actions to post new animal

--- src/redux/actions/animalActions.js
+++ src/redux/actions/animalActions.js
@@ -9,6 +9,22 @@ export const fetchAnimal = createAsyncThunk("user/fetchAnimal", async () => {
       Authorization: `Bearer ${authToken}`,
     },
   });
-  // console.log(response.data)
+  console.log(response.data)
   return response.data;
 });
+
+export const addAnimal = createAsyncThunk("user/addAnimal", async (formData) => {
+  try {
+    const authToken = localStorage.getItem('authToken');
+    const response = await axios.post(`${apiURL}/animals`, formData, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    console.log(response.data);
+    return response.data
+  } catch (error) {
+    console.error("Error adding animal:", error);
+    throw error
+  }
+})
