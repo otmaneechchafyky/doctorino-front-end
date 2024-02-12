3de26b1631dfab9906422fdb058cb0ae23a89aa4
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-12 06:45:52 +0100

    Update genre actions to fetch all genres

--- src/redux/actions/genreActions.js
+++ src/redux/actions/genreActions.js
@@ -2,9 +2,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { apiURL } from "./URL";
 
-const authToken = localStorage.getItem('authToken');
-
 export const fetchGenre = createAsyncThunk("user/fetchGenre", async () => {
+  const authToken = localStorage.getItem('authToken');
   const response = await axios.get(`${apiURL}/genres`, {
     headers: {
       Authorization: `Bearer ${authToken}`,
