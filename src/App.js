a064bdb0c6bf247d646bf1930a6431e7ec561631
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-13 05:56:43 +0100

    Run build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import SignUpForm from './components/forms/SignUpForm'
 import LoginForm from './components/forms/LoginForm';
-// import Home from './routes/Home'
 import Animals from './routes/Animals'
 import Vets from './routes/Vets'
 import Appointments from './routes/Appointments'
 import NewAnimal from './components/forms/NewAnimal'
 import NewVet from './components/forms/NewVet'
 import NewAppointment from './components/forms/NewAppointment'
+import Animal from "./routes/Animal";
 import './App.css';
 
 function App() {
@@ -15,9 +15,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<LoginForm />} />
-        {/* <Route path='/home' element={<Home />} /> */}
         <Route path='/signup' element={<SignUpForm />} />
         <Route path='/home/animals' element={<Animals />} />
+        <Route path='/home/animal/:id' element={<Animal />} />
         <Route path='/home/vets' element={<Vets />} />
         <Route path='/home/appointments' element={<Appointments />} />
         <Route path='/new_animal' element={<NewAnimal />} />
