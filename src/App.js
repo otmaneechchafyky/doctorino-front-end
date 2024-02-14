7ed03648586eb9422f1085d293d979e9afe9cc55
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-14 06:01:47 +0100

    Update App.js to include edit route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import SignUpForm from './components/forms/SignUpForm'
-import LoginForm from './components/forms/LoginForm';
-import Animals from './routes/Animals'
-import Vets from './routes/Vets'
-import Appointments from './routes/Appointments'
-import NewAnimal from './components/forms/NewAnimal'
-import NewVet from './components/forms/NewVet'
-import NewAppointment from './components/forms/NewAppointment'
+import { Routes, Route } from "react-router-dom";
+import SignUpForm from "./components/forms/SignUpForm";
+import LoginForm from "./components/forms/LoginForm";
+import Animals from "./routes/Animals";
+import Vets from "./routes/Vets";
+import Appointments from "./routes/Appointments";
+import NewAnimal from "./components/forms/NewAnimal";
+import NewVet from "./components/forms/NewVet";
+import NewAppointment from "./components/forms/NewAppointment";
+import EditAnimal from "./components/forms/editAnimal";
 import Animal from "./routes/Animal";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='/signup' element={<SignUpForm />} />
-        <Route path='/home/animals' element={<Animals />} />
-        <Route path='/home/animal/:id' element={<Animal />} />
-        <Route path='/home/vets' element={<Vets />} />
-        <Route path='/home/appointments' element={<Appointments />} />
-        <Route path='/new_animal' element={<NewAnimal />} />
-        <Route path='/new_vet' element={<NewVet />} />
-        <Route path='/new_appointment' element={<NewAppointment />} />
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/home/animals" element={<Animals />} />
+        <Route path="/home/animal/:id" element={<Animal />} />
+        <Route path="/home/edit_animal/:id" element={<EditAnimal />} />
+        <Route path="/home/vets" element={<Vets />} />
+        <Route path="/home/appointments" element={<Appointments />} />
+        <Route path="/new_animal" element={<NewAnimal />} />
+        <Route path="/new_vet" element={<NewVet />} />
+        <Route path="/new_appointment" element={<NewAppointment />} />
       </Routes>
     </div>
   );
