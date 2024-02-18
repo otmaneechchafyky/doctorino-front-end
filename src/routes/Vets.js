79b6849c88c8810fa2baffdce4bc8fdbadd0cddd
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-18 03:45:57 +0100

    Update vets component

--- src/routes/Vets.js
+++ src/routes/Vets.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
@@ -6,28 +6,61 @@ import { fetchVets } from "../redux/actions/vetActions";
 import { fetchSpecializations } from "../redux/actions/specializationActions";
 import { useDispatch, useSelector } from "react-redux";
 import { IoIosPricetag } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 
 const Vets = () => {
+
   const dispatch = useDispatch();
   const vetsList = useSelector((state) => state.vetsData.vetsArray);
   const specializationsList = useSelector(
     (state) => state.specializationsData.specializationArray
   );
 
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredVetsList, setFilteredVetsList] = useState(vetsList)
+
   useEffect(() => {
     dispatch(fetchVets());
-    console.log(vetsList)
-    // dispatch(fetchVet(3));
     dispatch(fetchSpecializations());
   }, [dispatch]);
+
+  useEffect(() => {
+    setFilteredVetsList(vetsList);
+  }, [vetsList]);
+
+  const searchVet = () => {
+    if (searchInput.trim() === "") {
+      setFilteredVetsList(vetsList);
+    } else {
+      const searchedVets = vetsList.filter(
+        (vet) =>
+          vet.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      setFilteredVetsList(searchedVets);
+    }
+  }
   return (
     <div className="flex w-full h-screen">
       <Navbar />
       <div className="bg-slate-900 flex flex-col w-[80%] text-white">
         <Header title="Vets" />
+        <div className="border rounded mx-auto flex w-[57%]">
+            <input
+              type="text"
+              className="px-4 py-2 w-[87%] text-slate-900 outline-0"
+              placeholder="Search by name..."
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button
+              className="flex items-center justify-center px-4 border-l w-[13%] hover:bg-slate-800"
+              onClick={searchVet}
+            >
+              <CiSearch className="w-7 h-7" />
+            </button>
+          </div>
         <ul className="py-6 grid place-items-center grid-cols-4 gap-y-8 overflow-y-scroll">
-          {vetsList &&
-            vetsList.map((vet) => (
+          {filteredVetsList &&
+            filteredVetsList.map((vet) => (
               <li key={vet.id} className="group flex flex-col items-center gap-2 p-4 rounded-lg bg-slate-800 hover:bg-slate-600 duration-300 hover:scale-105">
                 <img src={vet.vet_photo} alt={vet.name} className="w-48 rounded-[100%] border border-8 border-slate-700 group-hover:border-slate-500"/>
                 <div className="flex flex-col items-center">
@@ -40,7 +73,7 @@ const Vets = () => {
                     })}
                 </div>
                 <p className="flex items-center gap-2 text-teal-400 group-hover:text-teal-300"><IoIosPricetag /> <span className="flex gap-1">{vet.fees}<span>DH</span></span> <span className="text-white text-sm">Per visit</span></p>
-                <Link to="/vet_details" className="cursor-pointer p-2 w-full bg-sky-700 group-hover:bg-sky-600 text-center rounded text-lg tracking-wide">vet's details</Link>
+                <Link to={`/home/vets/${vet.id}`} className="cursor-pointer p-2 w-full bg-sky-700 group-hover:bg-sky-600 text-center rounded text-lg tracking-wide">vet's details</Link>
               </li>
             ))}
         </ul>
