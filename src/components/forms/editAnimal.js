3e2f97a1fc211e70b7e74fe9e7f69481ad0c47eb
echchafykyotmane <otmaneechchafyky5@gmail.com>
2024-02-23 05:42:31 +0100

    Style edit animal form to fit mobile screen

--- src/components/forms/editAnimal.js
+++ src/components/forms/editAnimal.js
@@ -50,7 +50,7 @@ const EditAnimal = () => {
     return (
       <div className="flex w-full h-screen">
         <Navbar />
-        <div className="bg-slate-900 flex flex-col w-[80%] text-teal-400 text-white">
+        <div className="bg-slate-900 flex flex-col w-5/5 lg:w-4/5 text-teal-400 text-white">
           <Header title="Edit animal" />
           <p>Loading ..</p>
         </div>
@@ -71,9 +71,9 @@ const EditAnimal = () => {
   }
 
   return (
-    <div className="flex w-full h-screen">
+    <div className="flex flex-col lg:flex-row w-full h-screen">
       <Navbar />
-      <div className="bg-slate-900 flex flex-col gap-4 w-[80%] text-teal-400 text-white">
+      <div className="bg-slate-900 flex flex-col gap-4 w-5/5 lg:w-4/5 text-teal-400 text-white">
         <Header title="Edit animal" />
         <form
           onSubmit={handleSubmit}
